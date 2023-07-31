df21e6eee1d7581e7344d27e044cdf73aa35f763
Shiv Nandan Jha <neureto12@gmail.com>
2023-07-31 22:03:21 +0530

    Final Commit

--- jobPortalWebsite/src/components/Navbar.jsx
+++ jobPortalWebsite/src/components/Navbar.jsx
@@ -5,13 +5,13 @@ import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="logo">UFoundIT</div>
-      <ul className="nav-links">
-        <li><a href="#about">About Us</a></li>
-        <li><a href="#job-seekers">For Job Seekers</a></li>
-        <li><a href="#companies">For Companies</a></li>
-        <li><a href="#contact">Contact Us</a></li>
+    <nav   className = "navbar">
+    <div   className = "logo">UFoundIT <span>:</span></div>
+    <ul    className = "nav-links">
+    <li><a href      = "#about">About Us</a></li>
+    <li><a href      = "#job-seekers">For Job Seekers</a></li>
+    <li><a href      = "#companies">For Companies</a></li>
+    <li><a href      = "#contact">Contact Us</a></li>
       </ul>
       <div className="auth-buttons">
         <button className="login-button">Login</button>
